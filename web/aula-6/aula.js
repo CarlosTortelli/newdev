daf918e9b55b8c9b706738b3ac71847d37a50980
Carlos Tortelli <carlosdtortelli@gmail.com>
2022-05-24 21:31:01 -0300

    javascript

---
+++ web/aula-6/aula.js
@@ -0,0 +1,52 @@
+//javascript usa linguagem de programação
+
+//POO: programacao orientada a objeto
+// atributos e métodos(funcoes)
+
+// exemplos de atts: cor, altura, largura e etc...
+//ex de métodos: freiar, acelerar, etc...
+
+
+// a convenção da declaração de variável deve ser seguida:
+
+//variável não pode iniciar com números
+//variável não pode iniciar com carácteres inciais
+//variável não pode iniciar com maiúscula
+//variáveis devem conter um valor semântico 
+//funções devem seguir as mesmas convenções e devem ter valor semântico
+
+
+
+
+
+let nomeVariavel = 1;
+
+
+
+//primeira forma de criar uma função, atribuir ela a uma variavel
+let somaValores = function () {
+  console.log('somando valores');
+}
+
+//chamar 
+
+somaValores ();
+
+//named function, método 2
+
+function subtraiValores () {
+  console.log('subatrai')
+}
+
+subtraiValores();
+
+//método 3 
+
+const FolhadePapel = {
+  color: 'branco',
+   tamanho: 'a4'
+}
+
+console.log('--->', FolhadePapel);
+
+

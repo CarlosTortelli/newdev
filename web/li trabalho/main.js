729243dff67ed00053dcf44fd0f32a042bc4e69f
Carlos Tortelli <carlosdtortelli@gmail.com>
2022-06-21 21:32:43 -0300

    lista de afazeres

---
+++ web/li trabalho/main.js
@@ -0,0 +1,18 @@
+function click (item) {
+
+  const cupuaçu = document.getElementById('batata').value
+  const banana = document.querySelectorAll('ul')
+
+  if(li) {
+    li.remove()
+  }
+  else {
+
+    item.createElement('li')
+    li.innerHTML = cupuaçu
+  }
+
+
+  banana.appendChild('li')
+  
+}

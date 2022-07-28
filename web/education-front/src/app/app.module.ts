2e3e0288e812de26136f66f6146c14f679ca2dc0
Carlos Tortelli <carlosdtortelli@gmail.com>
2022-07-27 21:31:09 -0300

    projeto angular

---
+++ web/education-front/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { LessonComponent } from './components/lesson/lesson.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RoomComponent } from './components/room/room.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    LessonComponent,
+    HeaderComponent,
+    LogoComponent,
+    SidebarComponent,
+    RoomComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
